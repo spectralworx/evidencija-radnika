038c79dfa6d29d7d6980294ce83507412e767ca4
spectralworx <pavle.ciric.dev@gmail.com>
2025-10-27 23:56:35 +0100

    Promene u konfiguraciji

--- evidencija-frontend/vite.config.js
+++ evidencija-frontend/vite.config.js
@@ -4,8 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    allowedHosts: ['.ngrok-free.app'], // ✅ dozvoljava sve ngrok linkove
-    host: true, // omogućava pristup sa vanjske mreže
-  },
 })
